a8c8868328d905ae8095fdbaef831d3c21b4b843
Adam Wróbel <adam@fluxinc.ca>
2012-04-29 23:52:37 +0200

    Add condition config option that accepts a proc.

--- lib/rack-ntlm/ntlm.rb
+++ lib/rack-ntlm/ntlm.rb
@@ -3,13 +3,15 @@ require 'net/ntlm'
 
 module Rack
   class Ntlm
-    
+
     def initialize(app, config = {})
+      default_condition = lambda do |env| true end
       @app = app
       @config = {
         :uri_pattern => /\//,
         :port => 389,
-        :search_filter => "(sAMAccountName=%1)"
+        :search_filter => "(sAMAccountName=%1)",
+        :condition => default_condition
       }.merge(config)
     end
 
@@ -18,22 +20,25 @@ module Rack
       ldap.host = @config[:host]
       ldap.port = @config[:port]
       ldap.base = @config[:base]
-      ldap.auth @config[:auth][:username], @config[:auth][:password] if @config[:auth]
+      ldap.auth @config[:auth][:username], @config[:auth][:password] \
+        if @config[:auth]
       !ldap.search(:filter => @config[:search_filter].gsub("%1", user)).empty?
     rescue => e
       false
     end
 
     def call(env)
-      if env['PATH_INFO'] =~ @config[:uri_pattern] && env['HTTP_AUTHORIZATION'].blank?
+      if env['HTTP_AUTHORIZATION'].blank? &&
+         env['PATH_INFO'] =~ @config[:uri_pattern] &&
+         @config[:condition].call(env)
         return [401, {'WWW-Authenticate' => "NTLM"}, []]
       end
 
       if /^(NTLM|Negotiate) (.+)/ =~ env["HTTP_AUTHORIZATION"]
-
+        plain = Base64::decode64($2)
         message = Net::NTLM::Message.decode64($2)
 
-        if message.type == 1 
+        if message.type == 1
           type2 = Net::NTLM::Message::Type2.new
           return [401, {"WWW-Authenticate" => "NTLM " + type2.encode64}, []]
         end
@@ -46,11 +51,9 @@ module Rack
             return [401, {}, ["You are not authorized to see this page"]]
           end
         end
-    	end
+      end
 
       @app.call(env)
     end
-
   end
-
 end
