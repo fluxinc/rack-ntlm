ce4aab695cdc093403c82eedef25232756d55f69
Adam Wróbel <adam@fluxinc.ca>
2012-04-30 00:18:32 +0200

    Remove the fake authentication code.

--- lib/rack-ntlm/ntlm.rb
+++ lib/rack-ntlm/ntlm.rb
@@ -9,22 +9,8 @@ module Rack
       @app = app
       @config = {
         :uri_pattern => /\//,
-        :port => 389,
-        :search_filter => "(sAMAccountName=%1)",
         :condition => default_condition
       }.merge(config)
-    end
-
-    def auth(user)
-      ldap = Net::LDAP.new
-      ldap.host = @config[:host]
-      ldap.port = @config[:port]
-      ldap.base = @config[:base]
-      ldap.auth @config[:auth][:username], @config[:auth][:password] \
-        if @config[:auth]
-      !ldap.search(:filter => @config[:search_filter].gsub("%1", user)).empty?
-    rescue => e
-      false
     end
 
     def call(env)
@@ -38,6 +24,8 @@ module Rack
         plain = Base64::decode64($2)
         message = Net::NTLM::Message.decode64($2)
 
+        # XXX Implement actual authentication
+        # The challenge-response pair needs to be verified
         if message.type == 1
           type2 = Net::NTLM::Message::Type2.new
           return [401, {"WWW-Authenticate" => "NTLM " + type2.encode64}, []]
@@ -45,11 +33,7 @@ module Rack
 
         if message.type == 3 && env['PATH_INFO'] =~ @config[:uri_pattern]
           user = Net::NTLM::decode_utf16le(message.user)
-          if auth(user)
-            env['REMOTE_USER'] = user
-          else
-            return [401, {}, ["You are not authorized to see this page"]]
-          end
+          env['REMOTE_USER'] = user
         end
       end
 
